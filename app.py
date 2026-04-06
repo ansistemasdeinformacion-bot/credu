21bbfb418063c84759a7a0d4a96c82705e199d38
Jonier Gomez <mateogmsz633@gmail.com>
2026-04-06 17:41:51 -0500

    Fix puerto para Render (10000)

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ def consultar():
         print("ERROR:", e)
         return jsonify({"success": False})
 
-# IMPORTANTE PARA RENDER
+# 🔥 IMPORTANTE PARA RENDER - USA EL PUERTO CORRECTO
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
